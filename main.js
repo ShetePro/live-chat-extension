96fc1f76eb85769c9caeb79ee86689e70f64ce02
ShetePro <sheteprolin@gmail.com>
2024-07-31 13:30:35 +0800

    feat(popup): 生成渲染函数

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 import "./popup.css";
-import { createDocumentEl } from "./src/utils/util";
+import {createDocumentEl, getConfig} from "./src/utils/util";
 import i18next from "i18next";
 import { setI18nConfig } from "./src/locales/i8n";
-import {ExtensionConfig} from "./src/config";
-setI18nConfig();
-renderSetting();
+import {createColorSetting, createLanguagesSetting, createTitle} from "./src/popup/popup";
+export let config = {}
+getConfig().then(({value}) => {
+  config = value
+  setI18nConfig({
+    lng: config.language
+  });
+  renderSetting()
+})
 i18next.on("languageChanged", function (lng) {
   console.log("language change", lng);
   renderSetting();
@@ -17,47 +23,12 @@ function renderSetting() {
   }
   const list = createDocumentEl("div", {
     classList: ["setting-list"],
-    append: [createColorSetting(), createLanguagesSetting()],
+    append: [createTitle(), createColorSetting(), createLanguagesSetting()],
   });
   main.append(list);
 }
 
-function createColorSetting() {
-  console.log(i18next);
-  const color = createDocumentEl("input");
-  color.type = "color";
-  color.value = ExtensionConfig.selectColor
-  return renderSettingItem(i18next.t("selectTextColor"), color);
-}
-function createLanguagesSetting() {
-  const select = createDocumentEl("select");
-  const options = [
-    { name: "English", value: "en" },
-    { name: "简体中文", value: "cn" },
-  ];
-  options.forEach((item) => {
-    const option = createDocumentEl("option", { append: [item.name] });
-    option.value = item.value;
-    option.name = item.name;
-    option.selected = item.value === ExtensionConfig.language
-    select.append(option);
-  });
-  select.addEventListener("change", (e) => {
-    console.log("change language", e.target.value);
-    ExtensionConfig.language = e.target.value
-    setI18nConfig({
-      lng: e.target.value,
-    });
-  });
-  return renderSettingItem(i18next.t("languages"), select);
-}
-function renderSettingItem(label, select) {
-  const span = createDocumentEl("span", { append: [label] });
-  return createDocumentEl("div", {
-    classList: ["setting-item"],
-    append: [span, select],
-  });
-}
+
 function changeLanguage(e) {
   console.log(e);
 }
--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,1 +1,54 @@
-
+import { createDocumentEl } from "../utils/util";
+import { ExtensionConfig } from "../background/config";
+import { i18Text, setI18nConfig } from "../locales/i8n";
+import {config} from "../../main";
+export function createTitle() {
+  return createDocumentEl("div", {
+    classList: ["title"],
+    append: [i18Text("setting")],
+  });
+}
+export function createColorSetting() {
+  const color = createDocumentEl("input");
+  color.type = "color";
+  color.value = ExtensionConfig.selectColor;
+  return renderSettingItem(i18Text("selectTextColor"), color);
+}
+export function createLanguagesSetting() {
+  const select = createDocumentEl("select");
+  const options = [
+    { name: "English", value: "en" },
+    { name: "简体中文", value: "cn" },
+  ];
+  options.forEach((item) => {
+    const option = createDocumentEl("option", { append: [item.name] });
+    option.value = item.value;
+    option.name = item.name;
+    option.selected = item.value === config.language;
+    select.append(option);
+  });
+  select.addEventListener("change", (e) => {
+    config.language = e.target.value;
+    chrome.runtime.sendMessage(
+      {
+        type: "setData",
+        key: "config",
+        value: { ...config },
+      },
+      (response) => {
+        console.log(response.status);
+      },
+    );
+    setI18nConfig({
+      lng: e.target.value,
+    });
+  });
+  return renderSettingItem(i18Text("languages"), select);
+}
+function renderSettingItem(label, select) {
+  const span = createDocumentEl("span", { append: [label] });
+  return createDocumentEl("div", {
+    classList: ["setting-item"],
+    append: [span, select],
+  });
+}
