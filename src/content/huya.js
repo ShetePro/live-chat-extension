f0ec13ea2fbecb583e7648ca03c2f726e7e1e2b9
ShetePro <sheteprolin@gmail.com>
2024-09-02 22:21:19 +0800

    Revert "perf(content): 优化clearRemoveMsgNode 函数执行时机 只在查询时生效"
    This reverts commit ad05ea8c66e26b988ad49e499ba82a4eb5732086.

--- src/content/huya.js
+++ src/content/huya.js
@@ -4,7 +4,7 @@ import { getConfig, highLightText, observerListPush } from "../utils/util";
 import { watchConfig } from "../utils/configWatcher";
 import { setI18nConfig } from "../locales/i8n";
 import { LiveSearch } from "./LiveSearch";
-import { SiteType } from "../utils/enum";
+import {SiteType} from "../utils/enum";
 
 let contentConfig = {};
 let liveControl = null;
@@ -40,71 +40,40 @@ export class HuyaSearch extends LiveSearch {
     this.siteType = SiteType.huya;
     this.liveId = this.href[0];
     this.chatListDom = document.querySelector(this.listSelector);
-    this.init();
+    this.init()
   }
-
+  
   getNameSpanByMsg(msg) {
-    return msg?.querySelector(".name");
+    return msg?.querySelector('.name')
   }
   // 获取内容span
   getChatSpanByMsg(msg) {
-    return msg?.querySelector(".msg");
+    return msg?.querySelector('.msg')
   }
   // 重写查询定位
   scrollTo(index) {
     return new Promise((resolve, reject) => {
       const textDom = this.searchList[index - 1];
       if (!textDom) {
-        console.log("index 错误", index);
+        console.log("index 错误");
         reject();
         return;
       }
       const scrollBar = this.getScrollBar();
       this.searchTextTop = textDom.offsetTop;
-      const offset = Math.max(0, textDom.offsetTop - 300);
-      const top =
-        this.chatListDom.style.top === "auto"
-          ? this.chatListDom.offsetHeight
-          : parseInt(this.chatListDom.style.top);
-      this.scrollView(textDom, offset <= Math.abs(top) ? -1 : 1);
-      // // 创建WheelEvent对象
-      // const event = new WheelEvent("wheel", {
-      //   bubbles: true,
-      //   cancelable: true,
-      //   deltaX: 0,
-      //   deltaY: -2, // 向上滚动
-      //   deltaMode: WheelEvent.DOM_DELTA_PAGE,
-      // });
-      // this.chatListDom.dispatchEvent(event);
+      const top = isNaN(parseInt(scrollBar.style.top))
+        ? 0
+        : parseInt(scrollBar.style.top);
+      // 创建WheelEvent对象
+      const event = new WheelEvent("wheel", {
+        bubbles: true,
+        cancelable: true,
+        deltaX: 0,
+        deltaY: -100, // 向上滚动
+        deltaMode: WheelEvent.DOM_DELTA_PIXEL,
+      });
+      this.chatListDom.dispatchEvent(event);
       resolve();
     });
-  }
-  scrollView(textDom, direction = -1) {
-    const offset = Math.max(0, textDom.offsetTop - 300);
-    const top =
-      this.chatListDom.style.top === "auto"
-        ? 0
-        : Math.abs(parseInt(this.chatListDom.style.top));
-    if (direction === -1 && this.chatListDom.style.top === "0px") {
-      return;
-    }
-    if (direction === 1 && this.chatListDom.style.top === "auto") {
-      return;
-    }
-    console.log(offset);
-    console.log(top);
-    console.log(direction)
-    const event = new WheelEvent("wheel", {
-      bubbles: true,
-      cancelable: true,
-      deltaX: 0,
-      deltaY: 120 * direction, // 向上滚动
-      deltaMode: WheelEvent.DOM_DELTA_PIXEL,
-    });
-    this.chatListDom.dispatchEvent(event);
-    console.log((direction === -1 ? offset <= top : offset >= top))
-    if (top === 0 || (direction === -1 ? offset <= top : offset >= top)) {
-      this.scrollView(textDom, direction);
-    }
   }
 }
