2a3aad55a2c0f038458ab05c8d8d41faf30d05f2
ShetePro <sheteprolin@gmail.com>
2024-09-18 17:46:59 +0800

    feat(popup): 更改环境配置 引入react 和 shadcn-ui 、tailwindcss 来开发

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import { crx } from "@crxjs/vite-plugin";
 import manifest from "./manifest.json";
-import vue from "@vitejs/plugin-vue";
+import react from "@vitejs/plugin-react";
+import path from "path"
 export default defineConfig({
   root: '.',
-  plugins: [vue(), crx({ manifest })],
+  plugins: [react(), crx({ manifest })],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
